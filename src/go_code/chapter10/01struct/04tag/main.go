735268112b136895b08e3e80e445008eebe98bd8
张梦思 <mengsi.zhang@emagames.cn>
2021-10-12 17:44:24 +0800

    tag

---
+++ src/go_code/chapter10/01struct/04tag/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Monster struct {
+	Name  string `json:"name"`
+	Age   int    `json:"age"`
+	Skill string `json:"skill"`
+}
+
+func main() {
+	monster := Monster{"牛魔王", 500, "芭蕉扇"}
+
+	jsonStr, err := json.Marshal(monster)
+	if err != nil {
+		fmt.Println("json 处理错误", err)
+	}
+	fmt.Println("jsonStr", string(jsonStr))
+}
