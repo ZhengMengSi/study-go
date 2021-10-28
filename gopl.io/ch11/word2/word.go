a6278dbbfe831cf8939130f1382082cb2ca9f52b
张梦思 <mengsi.zhang@emagames.cn>
2021-10-28 16:53:20 +0800

    fun

--- gopl.io/ch11/word2/word.go
+++ gopl.io/ch11/word2/word.go
@@ -1,6 +1,7 @@
 package word2
 
 import (
+	"strings"
 	"unicode"
 )
 
@@ -18,4 +19,17 @@ func IsPalindrome(s string) bool {
 		}
 	}
 	return true
+}
+
+// Split 把字符串s按照给定的分隔符sep进行分割返回字符串切片
+func Split(s, sep string) (result []string) {
+	i := strings.Index(s, sep)
+
+	for i > -1 {
+		result = append(result, s[:i])
+		s = s[i+len(sep):] // 这里使用len(sep)获取sep的长度
+		i = strings.Index(s, sep)
+	}
+	result = append(result, s)
+	return
 }
