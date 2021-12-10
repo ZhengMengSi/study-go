45b327908cd988ad334e147da2fe4cda4e3d5c40
张梦思 <547151644@qq.com>
2021-12-10 08:49:22 +0800

    1

--- gopl.io/ch01/echo1/main.go
+++ gopl.io/ch01/echo1/main.go
@@ -5,11 +5,21 @@ import (
 	"os"
 )
 
-func main() {
+/* func main() {
 	var s, sep string
+	fmt.Println(os.Args[0])
 	for i := 1; i < len(os.Args); i++ {
 		s += sep + os.Args[i]
 		sep = " "
+	}
+	fmt.Println(s)
+} */
+
+func main() {
+	s, sep := "", ""
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = ""
 	}
 	fmt.Println(s)
 }
